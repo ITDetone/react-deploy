12f278592c8271e51a007aa5309588b3fd4e27bc
E1125 <X353158@bcpcorp.net>
2020-10-20 11:15:02 +0100

    Updates

---
+++ precache-manifest.73b35f6784106e4fb2d26a21f5e561d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3c148a8763793932d453a1404d0b8f",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "57584125c3221e9720bd",
+    "url": "/react-deploy/static/css/main.14527b7c.chunk.css"
+  },
+  {
+    "revision": "ba7c17bae35ff1d449bd",
+    "url": "/react-deploy/static/js/2.8835a34d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-deploy/static/js/2.8835a34d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57584125c3221e9720bd",
+    "url": "/react-deploy/static/js/main.28505892.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
